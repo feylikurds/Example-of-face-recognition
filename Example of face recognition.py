0bf02621037520ee15459162ffdcbbd4f88f3513
feylikurds <feylikurds@gmail.com>
2023-02-12 19:42:10 +0100

    First commit

---
+++ Example of face recognition.py
@@ -0,0 +1,26 @@
+"""
+Example of face recognition.
+Copyright (C) 2023 Aryo Pehlewan aryopehlewan@hotmail.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from yoloface import face_analysis
+import numpy
+import cv2
+
+face=face_analysis()
+
+%%time
+img,box,conf=face.face_detection(image_path='input.jpg',model='tiny')
+print(box)                  # box[i]=[x,y,w,h]
+print(conf)                 # value between(0 - 1)  or probability
+face.show_output(img,box)
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Example-of-face-recognition
 Example of face recognition using machine/deep learning
+
+![Screenshot](result.png)
+
+## Downloads
+The source code can be downloaded as a Jupyter notebook or Python.
+
+## Purpose
+To show others an example of machine/deep-learning.
+
+## License
+Copyright (c) 2023 Aryo Pehlewan aryopehlewan@hotmail.com 
+Licensed under the GPL license.
+
